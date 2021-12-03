9694f6a31f615a60c143a1b9545f1ae61898edd6
edison.oda@hotmail.com <edison.oda@hotmail.com>
2021-12-03 18:52:21 -0300

    Show

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -3,28 +3,34 @@
 @section('content')
 <v-card
     class="mx-auto"
-    max-width="250"
-    dark
-    href="/products/{{$product->id}}"
-    hover
-    height="400">
+    dark>
+    <v-row no-gutters>
+        <v-col >
+            <v-img 
+            alt="`{{$product->title}}`"
+            src="{{$product->image_path}}"
+            max-height="500"></v-img>
+        </v-col>
+        <v-col align-self="end">
+            <v-card-title
+                class="text-h2"
+            >{{$product->title}}</v-card-title>
 
-    <v-img
-    alt="`{{$product->title}}`"
-    src="{{$product->image_path}}"
-    max-height="200"
-    ></v-img>
+            <v-card-subtitle class="brMoney text-h4">{{$product->price}}</v-card-subtitle>
 
-    <v-card-title class="text-h6">
-        {{$product->title}}
-    </v-card-title>
+            <v-card-text class="text-h6">
+                {{$product->description}}
+            </v-card-text>
 
-    <v-card-text>
-        <div class="brMoney">{{$product->price}}</div>
-    </v-card-text>
-
-    <v-card-text>
-        <div>{{$product->description}}</div>
-    </v-card-text>
+            <v-card-text>
+                <v-btn
+                outlined
+                rounded
+                >
+                Comprar
+                </v-btn>
+            </v-card-text>
+        </v-col>
+    </v-row>
 </v-card>
 @endsection
